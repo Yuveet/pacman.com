ebb88b5b42726cb8a887dddd6f5790fe042b4a56
Yuveet <poojassancheti@gmail.com>
2023-10-30 22:46:49 +0530

    New files

---
+++ src/Game.js
@@ -0,0 +1,173 @@
+import TileMap from "./TileMap.js";
+class Timer {
+  constructor(root) {
+    root.innerHTML = Timer.getHTML();
+
+    this.el = {
+      minutes: root.querySelector(".timer__part--minutes"),
+      seconds: root.querySelector(".timer__part--seconds"),
+      control: root.querySelector(".timer__btn--control"),
+      reset: root.querySelector(".timer__btn--reset")
+    };
+
+    this.interval = null;
+    this.remainingSeconds = 0;
+
+    this.el.control.addEventListener("click", () => {
+      if (this.interval === null) {
+        this.start();
+      } else {
+        this.stop();
+      }
+    });
+
+    this.el.reset.addEventListener("click", () => {
+      const inputMinutes = prompt("Enter number of minutes:");
+
+      if (inputMinutes < 60) {
+        this.stop();
+        this.remainingSeconds = inputMinutes * 60;
+        this.updateInterfaceTime();
+      }
+    });
+  }
+
+  updateInterfaceTime() {
+    const minutes = Math.floor(this.remainingSeconds / 60);
+    const seconds = this.remainingSeconds % 60;
+
+    this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+    this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+  }
+
+  updateInterfaceControls() {
+    if (this.interval === null) {
+      this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+      this.el.control.classList.add("timer__btn--start");
+      this.el.control.classList.remove("timer__btn--stop");
+    } else {
+      this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+      this.el.control.classList.add("timer__btn--stop");
+      this.el.control.classList.remove("timer__btn--start");
+    }
+  }
+
+  start() {
+    if (this.remainingSeconds === 0) return;
+
+    this.interval = setInterval(() => {
+      this.remainingSeconds--;
+      this.updateInterfaceTime();
+
+      if (this.remainingSeconds === 0) {
+        this.stop();
+      }
+    }, 1000);
+
+    this.updateInterfaceControls();
+  }
+
+  stop() {
+    clearInterval(this.interval);
+
+    this.interval = null;
+
+    this.updateInterfaceControls();
+  }
+
+  static getHTML() {
+    return `
+			<span class="timer__part timer__part--minutes">00</span>
+			<span class="timer__part">:</span>
+			<span class="timer__part timer__part--seconds">00</span>
+			<button type="button" class="timer__btn timer__btn--control timer__btn--start">
+				<span class="material-icons">play_arrow</span>
+			</button>
+			<button type="button" class="timer__btn timer__btn--reset">
+				<span class="material-icons">timer</span>
+			</button>
+		`;
+  }
+  
+}
+
+new Timer(
+	document.querySelector(".timer")
+);
+
+const tileSize = 32;
+const velocity = 2;
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const tileMap = new TileMap(tileSize);
+const pacman = tileMap.getPacman(velocity);
+const enemies = tileMap.getEnemies(velocity);
+
+let gameOver = false;
+let gameWin = false;
+const gameOverSound = new Audio("sounds/gameOver.wav");
+const gameWinSound = new Audio("sounds/gameWin.wav");
+
+function gameLoop() {
+  tileMap.draw(ctx);
+  drawGameEnd();
+  pacman.draw(ctx, pause(), enemies);
+  enemies.forEach((enemy) => enemy.draw(ctx, pause(), pacman));
+  checkGameOver();
+  checkGameWin();
+}
+
+function checkGameWin() {
+  if (!gameWin) {
+    gameWin = tileMap.didWin();
+    if (gameWin) {
+      gameWinSound.play();
+    }
+  }
+}
+
+function checkGameOver() {
+  if (!gameOver) {
+    gameOver = isGameOver();
+    if (gameOver) {
+      gameOverSound.play();
+    }
+  }
+}
+
+
+
+function isGameOver() {
+  return enemies.some(
+    (enemy) => !pacman.powerDotActive && enemy.collideWith(pacman)
+  );
+}
+
+function pause() {
+  return !pacman.madeFirstMove || gameOver || gameWin;
+}
+
+function drawGameEnd() {
+  if (gameOver || gameWin) {
+    let text = " You Win!";
+    if (gameOver) {
+      text = "Game Over";
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, canvas.height / 3.2, canvas.width, 80);
+
+    ctx.font = "75px comic sans";
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+
+    ctx.fillStyle = gradient;
+    ctx.fillText(text, 10, canvas.height / 2);
+  }
+}
+
+tileMap.setCanvasSize(canvas);
+setInterval(gameLoop, 1000 / 75);
